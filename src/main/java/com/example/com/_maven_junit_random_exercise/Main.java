bf2ac72183ba35fdb3343a30883932ca9242e4c3
Jenny Östman <jenny.ostman@yh.nackademin.se>
2019-08-29 09:28:44 +0200

    added in main

--- src/main/java/com/example/com/_maven_junit_random_exercise/Main.java
+++ src/main/java/com/example/com/_maven_junit_random_exercise/Main.java
@@ -8,6 +8,15 @@ public class Main
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        MyRandomMethods mrm = new MyRandomMethods();
+        
+        for(int i = 0; i < 5; i++) {
+        	System.out.println("value mellan 1-10: " + mrm.randomBetween1And10());
+        	System.out.println("value mellan 11-20: " + mrm.randomBetween11And20());
+        	System.out.println("value mellan 21-30: " + mrm.randomBetween21And30());
+        	System.out.println("value mellan 31-40: " + mrm.randomBetween31And40());
+        	System.out.println("value mellan 41-50: " + mrm.randomBetween41And50());
+        }
     }
+    
 }
